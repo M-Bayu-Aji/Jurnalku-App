d03382f1ac1a9d8c92af80cb79d906594bdb33a4
AzrielDanu <jiellowkey777@gmail.com>
2025-11-21 08:00:18 +0700

    update card in student behaviour

--- lib/screens/studentbehaviour.dart
+++ lib/screens/studentbehaviour.dart
@@ -49,6 +49,99 @@ class Studentbehaviour extends StatelessWidget {
           ],
         ),
       ),
+
+      body: Column(
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsetsGeometry.symmetric(horizontal: 15.0, vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Catatan Sikap saya',
+                          style: GoogleFonts.poppins(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey[900],
+                          ),
+                        ),
+                        Text(
+                          'Lihat catatan sikap dan perilaku yang telah dilaporkan',
+                          style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Padding(padding: const EdgeInsets.symmetric(vertical: 15),
+                        child: Container(
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFFFF8E5), // kuning muda
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: Color(0xFFFFCC75), // kuning border
+                              width: 1.5,
+                            ),
+                          ),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Icon(
+                                Icons.warning_amber_rounded,
+                                color: Color(0xFFFFA800),
+                                size: 26,
+                              ),
+
+                              SizedBox(width: 12),
+
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Perhatian",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600,
+                                        color: Color(0xFFB87B00),
+                                      ),
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      "Jika Anda merasa ada catatan yang tidak sesuai atau keliru, "
+                                      "silakan hubungi guru jurusan untuk mengajukan klarifikasi.",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        height: 1.4,
+                                        color: Color(0xFF6B4F00),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        ),
+                        
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
